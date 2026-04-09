aa68eddf4b3f8ccb3c0ae60f1a66d93b81d5360e
alnaimia <ali_alnaimi91@hotmail.com>
2026-04-09 16:03:43 +0100

    Standardize naming across launchers and update internal script paths

--- invoke_offboarding.bat
+++ invoke_offboarding.bat
@@ -5,7 +5,7 @@ REM Switch to the script folder safely, even if UNC path
 pushd "%~dp0"
 
 REM Now the current directory is mapped to a drive letter, so use %CD% to get the full path
-set "script=%CD%\source\ad_disable_staff_account.ps1"
+set "script=%CD%\source\invoke_offboarding.ps1"
 
 powershell -NoProfile -ExecutionPolicy Bypass ^
     "Start-Process PowerShell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -NoExit -File ""%script%""'"
